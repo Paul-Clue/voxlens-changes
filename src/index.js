c119e835473f362e1ecbb1f640d2eecfb2c92895
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2022-07-07 14:54:38 -0700

    Start from routes in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom";
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import { devToolsEnhancer } from 'redux-devtools-extension';
+// import rootReducer from './reducers';
+// import Routes from './components/Routes';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Routes />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
